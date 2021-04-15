ddc620b45409f1f5dabcac2b8958bffc41cbc790
Маша <mbanakova@yandex.ru>
2021-04-16 01:28:51 +0300

    build

---
+++ build/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+(function () {
+  let hotPrice = document.querySelector(".flat__value");
+  let developerPrice = document.querySelector(".flat__developer-value");
+
+  function splitPrice(elem) {
+    return elem.innerHTML.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  }
+
+  hotPrice.innerHTML = splitPrice(hotPrice);
+  developerPrice.innerHTML = splitPrice(developerPrice);
+})();
+//# sourceMappingURL=script.js.map
